0a57841ea43b758575690339649c1e13bc878add
riasingh23 <79793334+riasingh23@users.noreply.github.com>
2026-04-23 01:00:06 +0530

    shifting winner check to board from game && optimising board logic from O(N)

--- tic-tac-toe/src/main/java/com/tictactoe/Game.java
+++ tic-tac-toe/src/main/java/com/tictactoe/Game.java
@@ -3,7 +3,6 @@ package com.tictactoe;
 import com.tictactoe.entity.Board;
 import com.tictactoe.entity.Player;
 import com.tictactoe.enums.GameStatus;
-import com.tictactoe.enums.Piece;
 
 import java.util.List;
 
@@ -34,11 +33,11 @@ public class Game {
     }
 
     public void updateGame(int i, int j) {
-        if(isWinner(i, j, currPlayer.getPiece())) {
+        if(board.checkWinner(i, j, currPlayer.getPiece())) {
             winner = currPlayer;
             gameStatus = GameStatus.WIN;
         }
-        if(board.availableSpot().isEmpty()) {
+        if(board.isBoardFull()) {
             gameStatus = GameStatus.DRAW;
         }
     }
@@ -49,53 +48,12 @@ public class Game {
     }
 
     public void makeMove(int i, int j) {
-        boolean updated = board.updateBoard(i, j, currPlayer);
+        boolean updated = board.updateMoveOnBoard(i, j, currPlayer.getPiece());
         if(updated) {
             updateGame(i, j);
             if(GameStatus.IN_PROGRESS.equals(gameStatus)){
                 currPlayer = nextPlayer();
             }
         }
-    }
-
-    public boolean isWinner(int i, int j, Piece piece) {
-        int n=board.getSize();
-        if(i == j) {
-            boolean right_diag = true;
-            for(int r=0; r<n; r++) {
-                if(board.getPieceAtPosition(r,r) != piece){
-                    right_diag = false;
-                    break;
-                }
-
-            }
-            if(right_diag) return right_diag;
-        }
-        if(i+j==n) {
-            boolean left_diag = true;
-            for(int r=0; r<n; r++) {
-                if(board.getPieceAtPosition(r,n-r-1) != piece){
-                    left_diag = false;
-                    break;
-                }
-
-            }
-            if(left_diag) return left_diag;
-        }
-        boolean vertical = true, horizontal = true;
-        for(int r=0; r<n; r++) {
-            if(board.getPieceAtPosition(i,r) != piece){
-                vertical = false;
-                break;
-            }
-        }
-        for(int r=0; r<n; r++) {
-            if(board.getPieceAtPosition(i,r) != piece){
-                horizontal = false;
-                break;
-            }
-        }
-
-        return vertical || horizontal;
     }
 }
--- tic-tac-toe/src/main/java/com/tictactoe/entity/Board.java
+++ tic-tac-toe/src/main/java/com/tictactoe/entity/Board.java
@@ -2,25 +2,46 @@ package com.tictactoe.entity;
 
 import com.tictactoe.enums.Piece;
 import com.tictactoe.Pair;
-import javax.swing.*;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Board {
     private final Piece[][] board;
     private final int n;
+    private Map<Piece, Integer> diagCount;
+    private Map<Piece, Integer> antiDiagCount;
+    private Map<Piece, int[]> rowCount;
+    private Map<Piece, int[]> colCount;
+    private int totalMove;
 
     public Board(int n) {
         this.board = new Piece[n][n];
         this.n = n;
+        this.antiDiagCount = new HashMap<>();
+        this.diagCount = new HashMap<>();
+        this.rowCount = new HashMap<>();
+        this.colCount = new HashMap<>();
+        this.totalMove = 0;
     }
 
-    public boolean updateBoard(int i, int j, Player player) {
-        if(i<0 || j<0 || i>=n || j>=n || board[i][j]!=null) {
-            System.out.println("Invalid i: "+ i +", j: " + j);
-            return false;
+    public boolean updateMoveOnBoard(int row, int col, Piece piece) {
+        if(row<0 || col<0 || row>=n || col>=n) {
+            throw new IndexOutOfBoundsException("Move is out of range " + n);
         }
-        board[i][j] = player.getPiece();
+        if(board[row][col]!=null) {
+            throw new IllegalArgumentException("The cell: " + row + ", " + col + "is already filled");
+        }
+        board[row][col] = piece;
+        if(row == col) diagCount.put(piece, diagCount.getOrDefault(piece, 0) + 1);
+        if(row+col == n-1) antiDiagCount.put(piece, antiDiagCount.getOrDefault(piece, 0) + 1);
+        rowCount.put(piece,rowCount.getOrDefault(piece, new int[n]));
+        rowCount.get(piece)[row]++;
+        colCount.put(piece,colCount.getOrDefault(piece, new int[n]));
+        colCount.get(piece)[col]++;
+        totalMove++;
         return true;
     }
 
@@ -28,21 +49,8 @@ public class Board {
         return board[i][j];
     }
 
-    public int getSize() {
-        return n;
-    }
-
-    public List<Pair<Integer, Integer>> availableSpot() {
-        List<Pair<Integer, Integer>> availableSpot = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++) {
-                if(board[i][j] == null) {
-                    Pair<Integer, Integer> pair = new Pair<>(i,j);
-                    availableSpot.add(pair);
-                }
-            }
-        }
-        return availableSpot;
+    public boolean isBoardFull() {
+        return totalMove == n*n;
     }
 
     public void printBoard (){
@@ -56,5 +64,13 @@ public class Board {
             }
             System.out.println();
         }
+    }
+
+    public boolean checkWinner(int row, int col, Piece piece) {
+        if(diagCount.getOrDefault(piece,0)==n) return true;
+        if(antiDiagCount.getOrDefault(piece,0)==n) return true;
+        if(rowCount.get(piece)[row]==n) return true;
+        if(colCount.get(piece)[col]==n) return true;
+        return false;
     }
 }
