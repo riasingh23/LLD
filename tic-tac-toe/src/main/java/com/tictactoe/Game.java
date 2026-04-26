c708f104a35f744c7ff8e262f99b39b48feac9f8
riasingh23 <79793334+riasingh23@users.noreply.github.com>
2026-04-26 19:39:48 +0530

    No abstraction for Turn Management. it is present in Game violating SRP.

--- tic-tac-toe/src/main/java/com/tictactoe/Game.java
+++ tic-tac-toe/src/main/java/com/tictactoe/Game.java
@@ -3,8 +3,6 @@ package com.tictactoe;
 import com.tictactoe.entity.Board;
 import com.tictactoe.entity.Player;
 import com.tictactoe.enums.GameStatus;
-import com.tictactoe.observer.ConsoleGameObserver;
-import com.tictactoe.observer.GameObserver;
 import com.tictactoe.observer.GameObserverManager;
 
 import java.util.ArrayList;
@@ -12,21 +10,19 @@ import java.util.List;
 
 public class Game {
     private final Board board;
-    private final List<Player> players;
-    private Player currPlayer;
     private GameStatus gameStatus;
     private Player winner;
     private List<Move> moves;
     private GameRuleEngine gameRuleEngine;
     private GameObserverManager gameObserverManager;
     private final Object lock = new Object();
+    private TurnManager turnManager;
 
     public Game(List<Player> players, GameObserverManager gameObserverManager, int n) {
         this.board = new Board(n);
         this.gameRuleEngine = new GameRuleEngine(n);
-        this.players = players;
+        this.turnManager = new TurnManager(players);
         this.moves = new ArrayList<>();
-        this.currPlayer = players.get(0);
         this.gameStatus = GameStatus.IN_PROGRESS;
         this.gameObserverManager = gameObserverManager;
     }
@@ -38,6 +34,7 @@ public class Game {
     public void play() {
         while (GameStatus.IN_PROGRESS.equals(gameStatus)) {
             synchronized (lock) {
+                Player currPlayer = turnManager.getCurrentPlayer();
                 Move move = currPlayer.makeMove(board);
                 if (!board.validateMove(move)) {
                     gameObserverManager.notifyInvalidMove();
@@ -56,20 +53,15 @@ public class Game {
                     break;
                 }
                 gameObserverManager.notifyDisplayGame(board);
-                currPlayer = nextPlayer();
+                turnManager.moveToNextPlayer();
             }
             showWinner();
         }
     }
 
-    public Player nextPlayer() {
-        int index = players.indexOf(currPlayer);
-        return players.get((index+1)%(players.size()));
-    }
-
     public void showWinner() {
         if (GameStatus.WIN.equals(gameStatus)) {
-            gameObserverManager.notifyWin(currPlayer.getName());
+            gameObserverManager.notifyWin(winner.getName());
         } else if (GameStatus.DRAW.equals(gameStatus)) {
             gameObserverManager.notifyDraw();
         }
