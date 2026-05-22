efd662a1afd1de4ccb6a42bbedffdc0e8bad044b
riasingh23 <79793334+riasingh23@users.noreply.github.com>
2026-05-23 01:28:27 +0530

    Update App.java

--- amazon-locker/src/main/java/amazon/locker/App.java
+++ amazon-locker/src/main/java/amazon/locker/App.java
@@ -1,5 +1,12 @@
 package amazon.locker;
 
+import amazon.locker.entities.AmazonLocker;
+import amazon.locker.entities.Compartment;
+import amazon.locker.enums.CompartmentSize;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Compartment small = new Compartment("1", CompartmentSize.SMALL);
+        Compartment medium = new Compartment("2", CompartmentSize.MEDIUM);
+        Compartment large = new Compartment("3", CompartmentSize.LARGE);
+
+        List<Compartment> compartments = new ArrayList<>();
+        compartments.add(small);
+        compartments.add(medium);
+        compartments.add(large);
+
+        AmazonLocker amazonLocker = new AmazonLocker(compartments);
+
+        String codeSmall1 = amazonLocker.generateCodeAndPutPackage(CompartmentSize.SMALL);
+        System.out.println("Your parcel is kept in locker succesfully you can access with code: " + codeSmall1);
+
+        String codeSmall2 = amazonLocker.generateCodeAndPutPackage(CompartmentSize.SMALL);
+
     }
 }
